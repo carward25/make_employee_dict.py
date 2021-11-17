83d2b136d28b3d273fa471903468dd7ca44f4255
carward25 <carward@ucsc.edu>
2021-11-17 14:50:43 -0800

    amend

--- make_employee_dict.py
+++ make_employee_dict.py
@@ -1,26 +1,19 @@
 #name: Cassidy Ward
 #date: 11/17/2021
-#description: this program serves as a dictionary for storing employee names, salaries, ID number, and email.
-
-#Define the class Employee.
-
+#description: this program serves as a dictionary for storing employee names, salaries,
+# ID number, and email.
 class Employee():
     def __init__(self, name, Id, sal, email):
         self.employee_name = name
         self.ID_number = Id
         self.salary = sal
         self.email_address = email
-
-
 def make_employee_dict(names, ids, salaries, emails):
     employee_dict = {}
-
     for i in range(len(names)):
         emp = Employee(names[i], ids[i], salaries[i], emails[i])
         employee_dict[ids[i]] = emp
     return employee_dict
-
-
 def main():
     names = ['John', 'Peter', 'Marsha', 'Jason']
     ids = [1, 2, 3, 4]
@@ -29,6 +22,4 @@ def main():
 
     emp_dict = make_employee_dict(names,ids,salaries,emails)
     print(emp_dict)
-
-
 main()
